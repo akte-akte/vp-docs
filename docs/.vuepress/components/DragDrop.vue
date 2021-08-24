ea2e4a07d0044799993debf0b4ebb3190b3ad97d
akte-akte <kate@annex.com.au>
2021-08-24 19:22:32 +1000

    feat(dragdrop): add and register a custom component in markdown

---
+++ docs/.vuepress/components/DragDrop.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Render me: {{ message }} </p>
+</template>
+
+<script>
+export default {
+  name: 'DragDrop',
+  data() {
+    return {
+      message: 'good day vuers'
+    }
+  }
+}
+</script>
---
+++ docs/good-things/drag-drop.md
@@ -0,0 +1,7 @@
+# drag and drop
+
+there should be a component here:
+
+<DragDrop></DragDrop>
+
+:crossed_fingers:

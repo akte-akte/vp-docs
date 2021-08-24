46ea07c324b225dd06f5f36d687b51f8f57eafea
akte-akte <kate@annex.com.au>
2021-08-24 21:31:12 +1000

    feat(dragdrop): add enough styling to indicate drag  &  drop function

--- docs/.vuepress/components/DragDrop.vue
+++ docs/.vuepress/components/DragDrop.vue
@@ -1,13 +1,15 @@
 <template>
   <p>{{ message }} </p>
-  <draggable class="drag-area" :list="list" @change="log">
+  <draggable class="drag-area" ghost-class="ghost" :list="list" @change="log">
   <div
     class="list-styles"
     v-for="element in list"
     :key="element.name"
   >
-    {{ element.name }}
+    <strong>{{ element.name }}</strong>
+
   </div>
+
 </draggable>
 </template>
 
@@ -23,14 +25,38 @@ export default {
     return {
       message: 'Good day, vuers. Here is a list todo.',
       enabled: true,
+      hover: false,
       list: [
         { name: 'Add Sass', id: 1 },
         { name: 'Change the data source for this component', id: 2 },
         { name: 'Have a coffee', id: 3 },
         { name: 'Configure markdown linting to stop complining about inline html', id: 4 },
+        { name: 'Fix the colour contrast on this element when being dragged in dark :moon: mode', id: 5 },
+
       ],
       dragging: false
     }
   }
 }
 </script>
+
+<style lang="sass">
+.drag-area{
+  cursor: move;
+}
+
+.list-styles {
+  padding: 1.5rem;
+  margin: 0.5rem 0;
+  border: solid 2px orange;
+  font-weight: 600;
+  letter-spacing: .1em;
+  transition: text-shadow .3s;
+  & :hover {
+    text-shadow: 0 0 .65px #333, 0 0 .65px #333;
+  }
+}
+.ghost {
+  background-color: plum;
+}
+</style>

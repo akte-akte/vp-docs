78f4920a5b63fe654e904dd9843afe0bde1ae87d
akte-akte <kate@annex.com.au>
2021-08-25 07:23:04 +1000

    chore(linting): add linting for vue project

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    "plugin:vue/recommended",
+    "eslint:recommended",
+    "prettier/vue",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/html-closing-bracket-newline": ["off"],
+    "vue/max-attributes-per-line": ["off"]
+  }
+}
